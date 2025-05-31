73a5d6aabc44627c41674e4956b30106217a967d
rwneto3 <202503631611@alunos.estacio.br>
2025-05-31 03:00:12 +0000

    Reorganização de nomes e de pastas e arquivos

---
+++ Tema1/114-Variaveis-e-tipos-de-dados/variaveis_e_tipos_de_dados.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = "Rafael";
+    
+
+    
+return 0;
+}
